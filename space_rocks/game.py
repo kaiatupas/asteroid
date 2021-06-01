f255bf568cbeeec655e21d47c71f69bac1807535
kaiat <kaiatupas@gmail.com>
2021-06-01 17:05:43 +0800

    step 10 ending the game

--- space_rocks/game.py
+++ space_rocks/game.py
@@ -1,7 +1,7 @@
 import pygame
 
 from models import Asteroid, Spaceship
-from utils import get_random_position, load_sprite
+from utils import get_random_position, load_sprite, print_text
 
 class SpaceRocks:
     MIN_ASTEROID_DISTANCE = 250
@@ -11,6 +11,8 @@ class SpaceRocks:
         self.screen = pygame.display.set_mode((800, 600))
         self.background = load_sprite("space", False)
         self.clock = pygame.time.Clock()
+        self.font = pygame.font.Font(None, 64)
+        self.message = ""
 
         self.asteroids = []
         self.bullets = []
@@ -65,6 +67,7 @@ class SpaceRocks:
                 for asteroid in self.asteroids:
                     if asteroid.collides_with(self.spaceship):
                         self.spaceship = None
+                        self.message = "You lost!"
                         break
 
             for bullet in self.bullets[:]:
@@ -79,12 +82,18 @@ class SpaceRocks:
                 if not self.screen.get_rect().collidepoint(bullet.position):
                     self.bullets.remove(bullet)
 
+            if not self.asteroids and self.spaceship:
+                self.message = "You won!"
+
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
         #self.asteroid.draw(self.screen)
 
         for game_object in self._get_game_objects():
             game_object.draw(self.screen)
+
+        if self.message:
+            print_text(self.screen, self.message, self.font)
 
         pygame.display.flip()
         self.clock.tick(60)
